e3881e14fdfce72a2194d7e21e0957ffda4a64d1
Prakhar Agarwal <prakhara56@gmail.com>
2025-01-15 21:41:13 +0530

    chunking and embedding and readme

--- src/cine_notes/pipeline/pipe.py
+++ src/cine_notes/pipeline/pipe.py
@@ -11,14 +11,14 @@ load_dotenv()
 
 try:
     video_processor = VideoProcessor(
-        youtube_url="https://www.youtube.com/watch?v=qhomKbL-mHw",
+        youtube_url="https://www.youtube.com/watch?v=X7iIKPoZ0Sw",
         output_base_dir="./my_videos"
     )
     # video_processor.extract_audio(video_processor.download_video())
 
     video_processor.download_video()
 
-    response = HuggingFaceWhisperAPI().query(video_processor.extract_audio())
+    response = HuggingFaceWhisperAPI().query(video_processor.extract_audio(),split_time_min=10)
     logging.info(f"Transcription response status: {response['status']}\nlength of transcript: {response['word_count']} words")
     print(f"Transcription response status: {response['status']}\nlength of transcript: {response['word_count']} words")
     
